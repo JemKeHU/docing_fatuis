914c48410463c5122a8383ee5ae877d08f3cea41
JemKeHU <staslovel1338@gmail.com>
2026-01-11 17:34:08 +0000

    add __name__ == '__main__'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI(
     title="My API",
@@ -9,3 +10,6 @@ app = FastAPI(
 @app.get("/")
 async def main():
     return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
