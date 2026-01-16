35f909668c0b90969a9bc99d4cf13071f3d087d4
JemKeHU <staslovel1338@gmail.com>
2026-01-16 07:48:57 +0000

    change id generation in add_task endpoint

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, Field
 from uvicorn import run
-from random import randint
 
 my_app = FastAPI(
     title="Test API",
     description="Just testing my skills",
     version="0.0.1"
 )
+
+status.HTTP_418_IM_A_TEAPOT
 
 class STaskAdd(BaseModel):
     name: str = Field(min_length=2, max_length=100, description="Task Name")
@@ -48,7 +49,7 @@ async def get_tasks(limit: int = 10, offset: int = 0, keyword: str | None = None
 @my_app.post("/tasks", response_model=TaskCreateResponse, status_code=status.HTTP_201_CREATED)
 async def add_task(task: STaskAdd):
     task_dict = task.model_dump()
-    task_dict["id"] = randint(100, 999)
+    task_dict["id"] = len(fake_tasks_db) + 1
     fake_tasks_db.append(task_dict)
     return {
         "ok": True,
